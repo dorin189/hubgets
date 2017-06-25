17512d624e55bd035981437ebcf188994c932cba
dorin189 <doriniosifescu@192-168-0-102.rdsnet.ro>
2017-06-25 21:03:35 +0300

    add bootstrap validator

--- assets/javascript/bootstrap-validator.js
+++ assets/javascript/bootstrap-validator.js
@@ -1,67 +1,46 @@
-/**
- * Created by doriniosifescu on 25/06/2017.
- */
 $(document).ready(function() {
-    $('#signupForm').bootstrapValidator({
-        feedbackIcons: {
+    $('#registrationForm').formValidation({
+        framework: 'bootstrap',
+        icon: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
         },
+        err: {
+            // You can set it to popover
+            // The message then will be shown in Bootstrap popover
+            container: 'tooltip'
+        },
         fields: {
-            first_name: {
-                row: '.col-lg-6 col-xs-6',
+            email: {
                 validators: {
                     notEmpty: {
-                        message: 'Input field is required.'
-                    }
-                }
-            },
-            last_name: {
-                row: '.col-lg-6 col-xs-6',
-                validators: {
-                    notEmpty: {
-                        message: 'Input field is required.'
-                    }
-                }
-            },
-            company: {
-                validators: {
-                    notEmpty: {
-                        message: 'Input field is required'
-                    }
-                }
-            },
-            email_address: {
-                err: 'tooltip',
-                validators: {
-                    notEmpty: {
-                        message: 'Email field is required'
+                        message: 'The e-mail address is required'
                     },
                     emailAddress: {
-                        message: 'The value is not a valid email address'
+                        message: 'Is not a valid email address'
                     },
                     regexp: {
-                        regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$'
+                        regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
+                        message: 'Invalid e-mail address'
                     }
                 }
-            }
+            },
         }
     })
-        .on('err.validator.fv', function (e, data) {
-            // data.bv        --> The FormValidation.Base instance
-            // data.field     --> The field name
-            // data.element   --> The field element
-            // data.validator --> The current validator name
+        .on('err.field.fv', function(e, data) {
+            // Get the tooltip
+            var $icon = data.element.data('fv.icon'),
+                title = $icon.data('bs.tooltip').getTitle();
 
-            if (data.field === 'email') {
-                // The email field is not valid
-                data.element
-                    .data('fv.messages')
-                    // Hide all the messages
-                    .find('.help-block[data-fv-for="' + data.field + '"]').hide()
-                // Show only message associated with current validator
-                    .filter('[data-fv-validator="' + data.validator + '"]').show();
-            }
+            // Destroy the old tooltip and create a new one positioned to the right
+            $icon.tooltip('destroy').tooltip({
+                html: true,
+                placement: 'bottom',
+                title: title,
+                container: 'body'
+            });
         });
+
+
 });
